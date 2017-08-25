6b61e5b4f8df2f88740eba2be0f0f86619b1d4b7
yansongda <me@yansongda.cn>
2017-08-25 21:58:46 +0800

    unit tests

--- tests/Gateways/Alipay/AppGatewayTest.php
+++ tests/Gateways/Alipay/AppGatewayTest.php
@@ -2,3 +2,24 @@
 
 namespace Yansongda\Pay\Tests\Gateways\Alipay;
 
+use Mockery;
+use Yansongda\Pay\Tests\TestCase;
+use Yansongda\Pay\Gateways\AppGateway;
+
+class AppGatewayTest extends TestCase
+{
+    public function testPay()
+    {
+        $config = [
+            'app_id' => 'mock-app_id',
+            'ali_public_key' => 'mock-public_key',
+            'private_key' => 'mock-private_key',
+            'return_url' => 'http://mock.dev/return',
+            'notify_url' => 'http://mock.dev/notify',
+        ];
+
+        $gateway = Mockery::mock('AppGateway[pay]', [$config]);
+        $gateway->withAnyArgs();
+
+    }
+}
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -6,4 +6,8 @@ use PHPUnit\Framework\TestCase as PHPUnitTestCase;
 
 class TestCase extends PHPUnitTestCase
 {
+    public function tearDown()
+    {
+        \Mockery::close();
+    }
 }
